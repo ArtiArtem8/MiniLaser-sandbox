139782401f84858ee45900fcafdf1f5f9c7e7ac6
artiartem <a_shitikov8@mail.ru>
2024-03-16 20:46:55 +0700

    Laser feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 use std::collections::HashMap;
 
 use log::{error, info};
-use macroquad::color::{BLACK, BLUE, Color, GREEN, RED, SKYBLUE, WHITE};
-use macroquad::input::{is_mouse_button_pressed,
-                       is_mouse_button_released,
-                       mouse_position,
-                       MouseButton};
+use macroquad::color::{BLACK, Color, hsl_to_rgb, SKYBLUE, WHITE};
+use macroquad::hash;
+use macroquad::input::{is_key_down, is_mouse_button_pressed, is_mouse_button_released,
+                       KeyCode, mouse_position, MouseButton};
 use macroquad::math::Vec2;
-use macroquad::prelude::{draw_texture, ImageFormat};
-use macroquad::shapes::{draw_circle, draw_line, draw_poly_lines};
+use macroquad::prelude::ImageFormat;
+use macroquad::shapes::draw_line;
 use macroquad::texture::{draw_texture_ex,
                          DrawTextureParams,
-                         load_texture,
                          Texture2D};
+use macroquad::ui::{root_ui, widgets};
 
 #[derive(Clone, Default, Debug)]
 pub struct Node {
@@ -22,7 +21,15 @@ pub struct Node {
     default_radius: f32,
     is_hovered: bool,
     is_dragged: bool,
+    dragged_start_pos: Vec2,
 }
+
+#[derive(Debug, Clone, Copy)]
+pub enum EdgeState {
+    Reflective,
+    Absorptive,
+}
+
 
 #[derive(Clone, Debug)]
 pub struct Edge {
@@ -31,18 +38,26 @@ pub struct Edge {
     color: Color,
     thickness: f32,
     is_hovered: bool,
+    state: EdgeState,
 }
+
+pub struct Segment(Vec2, Vec2, EdgeState);
 
 impl Edge {
     pub fn new(a: usize, b: usize) -> Self {
-        Self { a, b, color: WHITE, thickness: 5.0, is_hovered: false }
+        Self { a, b, color: WHITE, thickness: 5.0, is_hovered: false, state: EdgeState::Reflective }
     }
 
     fn draw(&self, start: Vec2, end: Vec2) {
         draw_line(start.x, start.y, end.x, end.y, self.thickness, self.color);
     }
     pub(crate) fn update(&mut self, delta: f32) {
-        let target_color = if self.is_hovered { SKYBLUE } else { WHITE };
+        let target_color = if self.is_hovered { SKYBLUE } else {
+            match self.state {
+                EdgeState::Reflective => WHITE,
+                EdgeState::Absorptive => BLACK
+            }
+        };
         lerp_color_in_place(&mut self.color, target_color, delta / 0.10);
     }
 }
@@ -56,6 +71,7 @@ pub struct NodeNetwork {
     key: usize,
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Ray {
     origin: Vec2,
     direction: Vec2,
@@ -65,7 +81,6 @@ pub struct Laser {
     position: Vec2,
     direction: Vec2,
     ray: Ray,
-    color: Color,
     thickness: f32,
     texture: Texture2D,
 }
@@ -76,13 +91,30 @@ impl Laser {
             position,
             direction,
             ray: Ray { origin: position, direction },
-            color: RED,
             thickness: 5.0,
             texture: Texture2D::from_file_with_format(
                 include_bytes!("../assets/laser.png"),
                 Some(ImageFormat::Png),
             ),
         }
+    }
+
+    pub fn ui(&mut self) {
+        let mut rotation = self.ray.direction.y.atan2(self.ray.direction.x).to_degrees();
+        if rotation < 0.0 {
+            rotation += 360.0;
+        }
+        widgets::Window::new(hash!(), Vec2::new(0., 0.), Vec2::new(400., 100.))
+            .label("Laser")
+            .ui(&mut *root_ui(), |ui| {
+                ui.slider(hash!(), "pos x", 0.0f32..1000.0f32, &mut self.position.x);
+                ui.slider(hash!(), "pos y", 0.0f32..1000.0f32, &mut self.position.y);
+                ui.slider(hash!(), "rotation", 0.0f32..360.0f32, &mut rotation);
+                ui.slider(hash!(), "thickness", 0.01f32..10.0f32, &mut self.thickness);
+            });
+        self.direction = Vec2::from_angle(rotation.to_radians());
+        self.ray.origin = self.position;
+        self.ray.direction = self.direction;
     }
 
 
@@ -102,26 +134,70 @@ impl Laser {
             },
         );
     }
-    pub fn draw(&self) {
-        let end = self.ray.origin + self.ray.direction * 10_000.0;
-
-        draw_line(self.position.x, self.position.y, end.x, end.y, self.thickness, self.color);
+    pub fn draw(&mut self, other: &Vec<Segment>) {
+        self.ray.origin = self.position + self.direction * 40.0;
+        let (points, _) = self.collide_many(other);
+        let mut prev_pos = self.position;
+        let mut t = 0.0;
+        for pos in points.iter() {
+            draw_line(prev_pos.x, prev_pos.y, pos.x, pos.y, self.thickness,
+                      hsl_to_rgb(t / 1_000.0, 1.0, 0.5));
+            prev_pos = *pos;
+            t += 1.0;
+        }
+        // if points.len() < 1_000 {
+        //     // let end = prev_pos + direction * 10_000.0;
+        //     // draw_line(prev_pos.x, prev_pos.y, end.x, end.y, self.thickness, self.color);
+        // }
         self.draw_texture();
     }
 
-    pub fn collide_many(&self, other: &Vec<(Vec2, Vec2)>) {
-        for segment in other.iter() {
-            self.collide(*segment);
+    pub fn collide_many(&self, other: &Vec<Segment>) -> (Vec<Vec2>, Vec2) {
+        let mut collision_points: Vec<Vec2> = Vec::new();
+        let mut ray = self.ray;
+        for _ in 0..1000 {
+            let mut closest_point: Segment = Segment(ray.origin + ray.direction * 20_000.0,
+                                                     ray.direction,
+                                                     EdgeState::Reflective);
+
+            for segment in other.iter() {
+                match Self::collide(ray, (segment.0, segment.1)) {
+                    Some((col_position, col_reflection)) =>
+                        if closest_point.0.distance(ray.origin) > col_position.distance(ray.origin)
+                        {
+                            closest_point = Segment(col_position, col_reflection, segment.2);
+                        },
+                    None => (),
+                };
+            }
+            collision_points.push(closest_point.0);
+
+            match closest_point.2 {
+                EdgeState::Absorptive => break,
+                _ => (),
+            }
+            if closest_point.0.distance(ray.origin) > 10_000.0 {
+                break;
+            }
+            ray = Ray {
+                origin: closest_point.0 + closest_point.1 * 0.1,
+                direction: closest_point.1,
+            };
         }
+        return (collision_points, self.ray.direction);
     }
-    pub fn collide(&self, other: (Vec2, Vec2)) {
-        let collision = self.ray.collides_with(other);
-        if let Some(np) = collision {
-            let pos = np.0;
-            let normal = np.1;
-            // info!("Collision at {:?}", pos);
-            draw_circle(pos.x, pos.y, 10.0, BLUE);
+    pub fn collide(ray: Ray, other: (Vec2, Vec2)) -> Option<(Vec2, Vec2)> {
+        let collision = ray.collides_with(other);
+        if let Some((pos, normal)) = collision {
+            let reflection = -(2.0 * normal.dot(ray.direction) * normal - ray.direction);
+            // draw_line(pos.x, pos.y,
+            //           pos.x + reflection.x * 100.0,
+            //           pos.y + reflection.y * 100.0,
+            //           5.0, RED);
+            // draw_circle(pos.x, pos.y, 10.0, BLUE);
+            return Some((pos, reflection));
         }
+        None
     }
     pub fn look_at(&mut self, position: Vec2) {
         self.direction = position - self.position;
@@ -148,7 +224,7 @@ impl Ray {
             if normal_to_collision.dot(ray_dir) > 0.0 {
                 normal_to_collision = -normal_to_collision;
             }
-            draw_line(collision.x, collision.y, (collision.x + normal_to_collision.x * 100.0), (collision.y + normal_to_collision.y * 100.0), 5.0, GREEN);
+            // draw_line(collision.x, collision.y, (collision.x + normal_to_collision.x * 100.0), (collision.y + normal_to_collision.y * 100.0), 5.0, GREEN);
             Some((collision, normal_to_collision))
         } else {
             None
@@ -231,10 +307,13 @@ impl NodeNetwork {
             }
             self.dragged_node = None;
         }
+
+        let mut is_some_hovered_node = false;
         for (i, node) in self.nodes.iter_mut() {
             node.update(_delta);
             node.is_hovered = node.contains(mouse_pos);
             if node.is_hovered {
+                is_some_hovered_node = true;
                 if is_mouse_button_pressed(MouseButton::Left)
                     && !node.is_dragged
                     && self.dragged_node.is_none() {
@@ -256,12 +335,22 @@ impl NodeNetwork {
                 }
             };
             edge.is_hovered = Self::point_line_collision(mouse_pos, pos1, pos2, edge.thickness);
+
+            if edge.is_hovered && !is_some_hovered_node &&
+                is_mouse_button_pressed(MouseButton::Left) {
+                edge.state = match edge.state {
+                    EdgeState::Reflective => EdgeState::Absorptive,
+                    EdgeState::Absorptive => EdgeState::Reflective,
+                }
+            }
         }
     }
-    pub fn get_all_connections(&self) -> Vec<(Vec2, Vec2)> {
+    pub fn get_all_connections(&self) -> Vec<Segment> {
         let mut connections = Vec::with_capacity(self.connections.len());
         for edge in &self.connections {
-            connections.push((self.nodes[&edge.a].position, self.nodes[&edge.b].position));
+            connections.push(Segment(self.nodes[&edge.a].position,
+                                     self.nodes[&edge.b].position,
+                                     edge.state));
         }
         connections
     }
@@ -277,7 +366,9 @@ impl NodeNetwork {
         if self.selected_node.is_none() { return; }
         let mp = vec2tuple(mouse_position());
         let node = &self.nodes[&self.selected_node.unwrap()];
-        draw_line(mp.x, mp.y, node.position.x, node.position.y, 5.0, WHITE);
+        let mut new_mp = node.position;
+        new_mp = Self::ctrl_shift(mp, node, &mut new_mp);
+        draw_line(new_mp.x, new_mp.y, node.position.x, node.position.y, 5.0, WHITE);
     }
     fn handle_mouse(&mut self) {
         if is_mouse_button_pressed(MouseButton::Right) && self.dragged_node.is_none() {
@@ -291,6 +382,7 @@ impl NodeNetwork {
                     break;
                 }
             }
+
             if let Some(selected_index) = selected_index {
                 if self.selected_node == Some(selected_index) {
                     self.selected_node = None;
@@ -301,9 +393,16 @@ impl NodeNetwork {
                     self.selected_node = Some(selected_index);
                 }
             } else {
-                let node_index = self.add_node(vec2tuple(mouse_position()));
-
+                let mp = vec2tuple(mouse_position());
+                let mut new_mp = mp;
+                if self.selected_node.is_some() {
+                    let node = &self.nodes[&self.selected_node.unwrap()];
+                    new_mp = node.position;
+                    new_mp = Self::ctrl_shift(mp, node, &mut new_mp);
+                }
+                let node_index = self.add_node(new_mp);
                 if let Some(selected_index) = self.selected_node {
+                    info!("Adding connection from {selected_index} to {node_index}");
                     self.add_connection(selected_index, node_index);
                     self.selected_node = None;
                 }
@@ -332,6 +431,19 @@ impl NodeNetwork {
                 }
             }
         }
+    }
+
+    fn ctrl_shift(mp: Vec2, node: &Node, new_mp: &Vec2) -> Vec2 {
+        let mut new_mp = *new_mp;
+        if is_key_down(KeyCode::LeftControl) {
+            let diff = mp - node.position;
+            if diff.x.abs() > diff.y.abs() {
+                new_mp.x = mp.x
+            } else { new_mp.y = mp.y }
+        } else {
+            new_mp = mp;
+        }
+        new_mp
     }
     fn point_line_collision(point: Vec2, line_start: Vec2, line_end: Vec2, thickness: f32) -> bool {
         let distance = point_to_line_distance(point, line_start, line_end);
@@ -410,9 +522,18 @@ impl Node {
     fn handle_drag(&mut self, delta: f32) {
         if self.is_dragged {
             lerp_color_in_place(&mut self.color, WHITE, delta / 0.10);
-            self.position = vec2tuple(mouse_position());
+            let mouse_pos = vec2tuple(mouse_position());
+            if is_key_down(KeyCode::LeftControl) {
+                let diff = mouse_pos - self.dragged_start_pos;
+                if diff.x.abs() > diff.y.abs() {
+                    self.position.x = mouse_pos.x;
+                } else { self.position.y = mouse_pos.y; }
+            } else {
+                self.position = mouse_pos;
+            }
         } else {
             lerp_color_in_place(&mut self.color, WHITE, delta / 0.10);
+            self.dragged_start_pos = self.position;
         };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
+#![windows_subsystem = "windows"]
+
+
 use log::info;
 use macroquad::prelude::*;
 
 use ray_cast::{Laser, NodeNetwork};
 
-#[macroquad::main("RayCast")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "RayCast".to_owned(),
+        window_width: 1920 / 2,
+        window_height: 1080 / 2,
+        ..Default::default()
+    }
+}
+#[macroquad::main(window_conf)]
 async fn main() {
     env_logger::init();
     info!("Program started");
 
     let mut network = NodeNetwork::new().await;
-    let mut laser = Laser::new(vec2(screen_width()/2.0, screen_height()/2.0), vec2(0.0, -1.0));
-    // draw giant star using network
+    let mut laser = Laser::new(vec2(screen_width()/2.0, screen_height()/2.0), vec2(1.0, 0.0));
     
+    network.add_node(Vec2::new(0.0, 0.0));
+    network.add_node(Vec2::new(screen_width(), 0.0));
+    network.add_node(Vec2::new(0.0, screen_height()));
+    network.add_node(Vec2::new(screen_width(), screen_height()));
+    
+    /*
     let count = 1_000;
     for _ in 0..count {
         network.add_node(vec2(rand::gen_range(0.0, screen_width()), rand::gen_range(0.0, screen_height())));
@@ -24,25 +40,26 @@ async fn main() {
     for i in 0..count {
         network.add_connection(count, i);
     }
-    
-    
+    */
+
 
     let mut time_delta: f32;
+    let mut show_ui: bool = false;
     loop {
         clear_background(Color::from_hex(0x282A36));
+        
+        if is_key_pressed(KeyCode::Tab) { show_ui = !show_ui; }
+        
         time_delta = get_frame_time();
         network.update(time_delta);
         network.draw();
-        laser.draw();
-        laser.look_at(Vec2::from(mouse_position()));
-        laser.collide_many(&network.get_all_connections());
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-
+        laser.draw(&network.get_all_connections());
         draw_text(format!("Frame time: {time_delta}").as_str(), 20.0, 20.0, 30.0, DARKGRAY);
 
+        if show_ui {
+            laser.ui();
+        }
+        
         next_frame().await
     }
 }
